05c8d227fac552feb55458fb729e92a3cdbe0a76
Alessandro Galli <a.galli85@gmail.com>
2016-01-30 12:13:51 +0100

    bundle class test

---
+++ Tests/unit/InfluxDbBundleTest.php
@@ -0,0 +1,25 @@
+<?php
+//declare(strict_types=1);
+
+namespace Algatux\InfluxDbBundle\unit;
+
+use Algatux\InfluxDbBundle\DependencyInjection\InfluxDbExtension;
+use Algatux\InfluxDbBundle\InfluxDbBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InfluxDbBundleTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test_get_container_extension()
+    {
+
+        $bundle = new InfluxDbBundle();
+
+        $extension = $bundle->getContainerExtension();
+
+        $this->assertInstanceOf(Bundle::class, $bundle);
+        $this->assertInstanceOf(InfluxDbExtension::class, $extension);
+
+    }    
+    
+}
