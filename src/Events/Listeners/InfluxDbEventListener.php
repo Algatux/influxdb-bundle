bb792330d60e78f170cda1262d0898975dfb7f0f
Alessandro Galli <a.galli85@gmail.com>
2016-01-31 15:18:58 +0100

    [Directory Structure] - reorganization

---
+++ src/Events/Listeners/InfluxDbEventListener.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Algatux\InfluxDbBundle\Events\Listeners;
+
+/**
+ * Class InfluxDbEventListener
+ * @package Algatux\InfluxDbBundle\Events\Listeners
+ */
+class InfluxDbEventListener
+{
+}
