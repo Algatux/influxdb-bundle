0bbb29b800c91c8824d9ee4cfa89db14e88e9926
Alessandro Galli <a.galli85@gmail.com>
2016-02-07 19:42:27 +0100

    now texts loading listeners

--- tests/unit/DependencyInjection/InfluxDbExtensionTest.php
+++ tests/unit/DependencyInjection/InfluxDbExtensionTest.php
@@ -11,8 +11,18 @@ class InfluxDbExtensionTest extends \PHPUnit_Framework_TestCase
     public function test_load()
     {
 
+        $confTest = [
+            'influx_db' => [
+                'host' => "localhost",
+                'database' => "udp",
+                'udp_port' => "4444",
+                'http_port' => "8086",
+                'use_events' => true,
+            ]
+        ];
+
         $extension = new InfluxDbExtension();
-        $config = $extension->load([],new ContainerBuilder());
+        $config = $extension->load($confTest,new ContainerBuilder());
 
         $this->assertNotEmpty($config);
     }
