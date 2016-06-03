b9f221feadc87f473b662270cb35667ceeab70bc
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-06-03 14:54:27 +0200

    Fix and improve PHPDoc for PointsCollection

--- src/Model/PointsCollection.php
+++ src/Model/PointsCollection.php
@@ -6,19 +6,23 @@ namespace Algatux\InfluxDbBundle\Model;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use InfluxDB\Database;
+use InfluxDB\Point;
 
 /**
  * Class PointsCollection.
  */
 class PointsCollection extends ArrayCollection
 {
+    /**
+     * @var string
+     */
     public $precision;
 
     /**
      * Initializes a new ArrayCollection.
      *
-     * @param array  $elements
-     * @param string $precision
+     * @param Point[] $elements
+     * @param string  $precision
      */
     public function __construct(array $elements = [], string $precision = Database::PRECISION_SECONDS)
     {
@@ -27,7 +31,7 @@ class PointsCollection extends ArrayCollection
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getPrecision()
     {
